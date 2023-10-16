5fed1787538454a9944fe93c52df6e47cbde851f
John <johnnkonge2020@gmail.com>
2023-10-16 21:10:05 +0300

    added some js but still not there

--- ResortReservationSystem/index.js
+++ ResortReservationSystem/index.js
@@ -1,20 +1,6 @@
-/*$("document").ready(function(){
-    alert("Welcome to LAKE ELEMENTAITA RESORT")
-});
-$("#cnt").click(function(){
-    alert("Thank you. We will be in touch");
-});
-
-$("#cnt").click(function(){
-    $("input").replace();
-});
-$("#news").click(function(){
-    prompt("Our News Letter will be send to your E-Mail")
-});
-*/
 document.addEventListener('DOMContentLoaded', run)
 function run(){
-    alert('Welcome to Lake Elementaita Resort, the place to be!', setTimeout(40000))
+    alert('Welcome to Lake Elementaita Resort, the place to be!',(400000000))
 
 }
 document.getElementById('cnt').addEventListener('click', (e) =>{
@@ -27,4 +13,34 @@ document.getElementById('book').addEventListener('click', (e)=> {
 })
 document.getElementById('view-all').addEventListener('click',(e)=>{
     alert('View all of our room here')
+})
+
+const form = document.getElementById('form')
+const fname = document.getElementById('name')
+const email = document.getElementById('email')
+const comment = document.getElementById('comment')
+const formData = {
+    'fname.value': '',
+    'email.value': '',
+    'comment.value': '',
+}
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+
+    const connfigureObj = {
+        method:'POST',
+        header:{
+            'Content-Type': 'application/json',
+            Accept:'application/json'
+        },
+        body: JSON.stringify(formData)
+    }
+    fetch('htpp://localhost:3000/', connfigureObj)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        console.log(data)
+    })
 })
